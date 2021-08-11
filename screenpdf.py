8f1bc8db16eb133a0d7c8fee85c3b84f3d055be3
Erin Jossy <64970489+Cap10N3m0@users.noreply.github.com>
2021-08-11 12:51:34 +0530

    General

--- screenpdf.py
+++ screenpdf.py
@@ -7,7 +7,7 @@ from PIL import Image
 img1 = Image.open('Untitled.png').convert('RGB')
 
 
-t = os.listdir('/home/erin/Pictures/')
+t = os.listdir('Location of Pictures Directory')
 
 tdy = datetime.date.today()
 
@@ -26,24 +26,24 @@ for i in l:
     img_list.append(im)
 
 if period ==1:
-    img1.save('/home/erin/MEGAsync/Fifth Sem/Dynamics Of Machines/SS/'+str(tdy)+'.pdf',
+    img1.save('Location of 1st course/SS/'+str(tdy)+'.pdf',
               save_all=True, append_images=img_list)
 elif period ==2:
-    img1.save('/home/erin/MEGAsync/Fifth Sem/Control System Eng/SS/'+str(tdy)+'.pdf',
+    img1.save('Location of 2nd course/SS/'+str(tdy)+'.pdf',
               save_all=True, append_images=img_list)
 elif period ==3:
-    img1.save('/home/erin/MEGAsync/Fifth Sem/Management Of Production System/SS/'+str(tdy)+'.pdf',
+    img1.save('Location of 3rd course/SS/'+str(tdy)+'.pdf',
               save_all=True, append_images=img_list)
 elif period == 4:
-    img1.save('/home/erin/MEGAsync/Fifth Sem/Principle of Heat Transfer/SS/'+str(tdy)+'.pdf',
+    img1.save('Location of 4th course/SS/'+str(tdy)+'.pdf',
               save_all=True, append_images=img_list)
 elif period == 5:
-    img1.save('/home/erin/MEGAsync/Fifth Sem/Thermal Eng I/SS/'+str(tdy)+'.pdf',
+    img1.save('Location of 5th course/SS/'+str(tdy)+'.pdf',
               save_all=True, append_images=img_list)
 elif period == 6:
-    img1.save('/home/erin/MEGAsync/Fifth Sem/Manufacturing Science/SS/'+str(tdy)+'.pdf',
+    img1.save('Location of 6th course/SS/'+str(tdy)+'.pdf',
               save_all=True, append_images=img_list)
 
 for i in l:
-    os.unlink("/home/erin/Pictures/"+str(i))
+    os.unlink("Location of Pictures Directory/"+str(i))
 
